f39664ae39511aad44e7b259517f3d7df7d04030
agsdtm7 <marsono.a2@gmail.com>
2018-06-08 14:44:36 -0400

    added ADD & REMOVE comment route and functionality, tested and worked successfully.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -135,5 +135,73 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
 });
 
 
+// LECTURE #28 ADD & REMOVE COMMENTS
+// @route POST api/posts/comment/:id
+// @desc Add comment to post
+// @access Private
+
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // we use the post validation because it has the same fields as post
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // if any errors, send 400 with errors object and the operation is over
+        return res.status(400).json(errors);
+    }
+    // If it passes the validation then find the post by id then add the comment then save it. in case any error happens, display the error
+    Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+            // Add to comments array
+            post.comments.unshift(newComment);
+
+            // Save
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No posts found' }));
+});
+
+// LECTURE #28 ADD & REMOVE COMMENTS
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc REMOVE comment from posts
+// @access Private
+
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // we use the post validation because it has the same fields as post
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // if any errors, send 400 with errors object and the operation is over
+        return res.status(400).json(errors);
+    }
+    // If it passes the validation then find the post by id then add the comment then save it. in case any error happens, display the error
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check to see if comment exists
+            if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res.status(404).json({ commentnotexists: 'Comment does not exist' });
+            }
+
+            // Get remove index
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id);
+
+            // Splice comment out of array
+            post.comments.splice(removeIndex, 1);
+
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No posts found' }));
+});
+
+
 
 module.exports = router;
