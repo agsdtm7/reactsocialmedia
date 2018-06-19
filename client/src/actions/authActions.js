4511438fa3a28bef558ae133abcf0b98445f870c
agsdtm7 <marsono.a2@gmail.com>
2018-06-19 11:10:49 -0400

    created setAuthToken to deal with authorization token to every request if we are logged in

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // import { TEST_DISPATCH } from './types'; // THIS WAS COMMENTED AT #41
 import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
 // Register User
 export const registerUser = (userData, history) => dispatch => {
     // dispatch above was added at lecture #41
@@ -15,4 +16,23 @@ export const registerUser = (userData, history) => dispatch => {
             })
         );
 
+};
+
+// Login - Get User Token (lecture #42)
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to localStorage
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+        })
+        // taking care of the error
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            }));
 };
