eb7fc602fe455a6327207f8cf78b327e90f771cb
agsdtm7 <marsono.a2@gmail.com>
2018-06-22 14:35:26 -0400

    profile reducer and profile actions added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
-
+import profileReducer from './profileReducer';
 
 export default combineReducers({
     // !IMPORTANT! this means we assign the word 'auth' as the short for authReducer
     // So say we use anythink from from authReducer in our components, we'll use  'this.props.auth'
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    profile: profileReducer
 });
