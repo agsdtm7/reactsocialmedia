1b4d121be9a8852d2c387c2f47b028f41d2479b1
agsdtm7 <marsono.a2@gmail.com>
2018-07-02 13:29:39 -0400

    created postReducer.js component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
+import postReducer from './postReducer';
 
 export default combineReducers({
     // !IMPORTANT! this means we assign the word 'auth' as the short for authReducer
     // So say we use anythink from from authReducer in our components, we'll use  'this.props.auth'
     auth: authReducer,
     errors: errorReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    post: postReducer
 });
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    posts =[],
+    post = {},
+    loading = false
+};
+
+export default function () {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
