a116d94668cd8a1d9dd3f3c468f1e49291abe5b8
agsdtm7 <marsono.a2@gmail.com>
2018-07-06 14:00:37 -0400

    ready to deploy for the first time

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://agus:agus@ds047792.mlab.com:47792/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
